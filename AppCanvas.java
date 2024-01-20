8e3c81cd09242126429cce82ae68284e29c912fd
Matthew Goins <mgoins2@uco.edu>
2024-01-20 06:15:58 -0600

    Finished

--- AppCanvas.java
+++ AppCanvas.java
@@ -1,5 +1,25 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class AppCanvas extends JPanel {
-    
+
+    int randomNumber = 0;
+
+    public AppCanvas(){
+        setPreferredSize(new Dimension(300,250));
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); // very important
+        var font = new Font("Courier New", Font.BOLD, 24);
+        g.setFont(font);
+        g.setColor(Color.BLUE);
+        g.drawString("Lucky Number is:", 30, 100);
+        g.drawString("" + randomNumber, 30, 140);
+    }
 }
--- AppWindow.java
+++ AppWindow.java
@@ -1,8 +1,26 @@
+import java.awt.BorderLayout;
+import java.util.Random;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class AppWindow extends JFrame {
     
     public void init() {
         setTitle("My first Java Window");
+        var cp = getContentPane();
+
+        var canvas = new AppCanvas();
+        cp.add(canvas, BorderLayout.CENTER);
+
+        JButton button = new JButton("Lucky Number");
+        cp.add(button,BorderLayout.SOUTH);
+
+        button.addActionListener(e -> {
+            var r = new Random();
+            int n = r.nextInt(101);
+            canvas.randomNumber = n;
+            canvas.repaint();
+        });
     }
 }
